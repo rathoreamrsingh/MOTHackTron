d72bd383c6fd7985b9b5601a9adf4c5e787d0ff8
sudhanshugit <sudhanshu2222kota@gmail.com>
2019-08-10 12:17:14 +0530

    pojo class

---
+++ Server/Hackthon/src/main/java/com/hackthon/POJO/QueueServicePOJO.java
@@ -0,0 +1,11 @@
+package com.hackthon.POJO;
+import java.util.Queue;
+
+public class QueueServicePOJO {
+	
+	Queue q;
+	int maxSize;
+	String name;
+	
+
+}
--- Server/Hackthon/src/main/java/com/hackthon/serviceImpl/QueueServiceImpl.java
+++ Server/Hackthon/src/main/java/com/hackthon/serviceImpl/QueueServiceImpl.java
@@ -2,16 +2,31 @@ package com.hackthon.serviceImpl;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.*;
 
+import com.hackthon.POJO.QueueServicePOJO;
 import com.hackthon.service.QueueService;
 
 public class QueueServiceImpl  implements QueueService{
 	
-	private Map<String,String> queueMap = new HashMap<String,String>();
+	private ConcurrentHashMap<String,QueueServicePOJO> queueMap;
+	
+	public void QueueServiceImp() {
+		queueMap = new ConcurrentHashMap<String, QueueServicePOJO>();
+	}
+	
 	
 	public void add(String queueName, String message) {
 		
 		
 	}
+	
+	
+	public void addQueue(String queueName, int size ) {
+		
+	}
+	
+	public Array
 
 }
