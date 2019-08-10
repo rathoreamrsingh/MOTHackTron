0c7a22db2d0bb1cec2d7b213d3736737f67f24d9
Rajasekhar Kummari <rajasekharkummari@live.com>
2019-08-10 14:32:59 +0530

    added user routes

--- JS/qms/routes/queues.js
+++ JS/qms/routes/queues.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var qController = require('../controllers/queuesController');
+var queuesController = require('../controllers/queuesController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  qController.getAllQueues(req,res);
+  queuesController.getAllQueues(req,res);
 });
 router.post('/', function(req, res, next) {
-  qController.createQueue(req,res);
+  queuesController.createQueue(req,res);
 });
 
 module.exports = router;
