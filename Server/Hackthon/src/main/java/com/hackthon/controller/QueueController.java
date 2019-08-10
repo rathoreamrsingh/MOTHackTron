8edaeed4e08de35d717068b9872dc68af53c3c34
Rajesh <rajesh.rcr38@gmail.com>
2019-08-10 13:51:42 +0530

    Added all methods in controller

--- Server/Hackthon/src/main/java/com/hackthon/controller/QueueController.java
+++ Server/Hackthon/src/main/java/com/hackthon/controller/QueueController.java
@@ -1,10 +1,12 @@
 package com.hackthon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hackthon.POJO.QueueServicePOJO;
 import com.hackthon.service.QueueService;
 
 @RestController
@@ -13,9 +15,9 @@ public class QueueController {
 	@Autowired QueueService service;
 	
 	@RequestMapping("/addElement")
-	public String addElelement(@RequestParam(value="name") String name,
+	public String addElelement(@RequestParam(value="queueName") String queueName,
 			@RequestParam(value="message") String message) {
-		Integer response =service.addMessageToQueue(name, message);
+		Integer response =service.addMessageToQueue(queueName, message);
 		
 		if(response==1) {
 			return "failure";
@@ -24,7 +26,56 @@ public class QueueController {
 		}
 		
 	}
+	
+	@RequestMapping("/deleteQueue")
+	public String deleteQueue(@RequestParam(value="queueName") String queueName) {
+		int response = service.deleteQueue(queueName);
+		if(response==1) {
+			return "failure";
+		} else {
+			return "success";
+		}
+	}
 
+	
+	
+	
+	
+	@RequestMapping("/addQueue")
+	public String addQueue(@RequestParam(value="queueName") String queueName, @RequestParam(value="maxSize") Integer maxSize) {
+		int response = service.addQueue( queueName,  maxSize);
+		if(response==1) {
+			return "failure";
+		} else {
+			return "success";
+		}
+	}
+	
+	
+	@RequestMapping("/removeAll")
+	public String removeAll() {
+		int response = service.removeAll();
+		if(response==1) {
+			return "failure";
+		} else {
+			return "success";
+		}
+	}
+	
+	@RequestMapping("/removeItemFromQueue")
+	public String removeItemFromQueue(@RequestParam(value="queueName") String queueName) {
+		int response = service.removeItemFromQueue(queueName);
+		if(response==1) {
+			return "failure";
+		} else {
+			return "success";
+		}
+	}
+	
+
+	
+	
+	
 	
 	
 	
