f5a3917be8e426802570a2f4e1e4156a0e95fdf8
sweet <sekhar1329@gmail.com>
2019-08-10 11:50:21 +0530

    adding service layer

--- Server/Hackthon/src/main/java/com/hackthon/service/QueueService.java
+++ Server/Hackthon/src/main/java/com/hackthon/service/QueueService.java
@@ -2,4 +2,5 @@ package com.hackthon.service;
 
 public interface QueueService {
 
+	void add(String queueName, String message);
 }
--- Server/Hackthon/src/main/java/com/hackthon/serviceImpl/QueueServiceImpl.java
+++ Server/Hackthon/src/main/java/com/hackthon/serviceImpl/QueueServiceImpl.java
@@ -1,7 +1,16 @@
 package com.hackthon.serviceImpl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.hackthon.service.QueueService;
 
 public class QueueServiceImpl  implements QueueService{
+	
+	private Map<String,String> queueMap = new HashMap<String,String>();
+	
+	public void add(String queueName, String message) {
+		
+	}
 
 }
