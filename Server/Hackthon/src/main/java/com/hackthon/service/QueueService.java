0adc3581e72de5391705fcf25b3db3e160b84a71
sudhanshugit <sudhanshu2222kota@gmail.com>
2019-08-10 13:36:15 +0530

    updated service file

--- Server/Hackthon/src/main/java/com/hackthon/service/QueueService.java
+++ Server/Hackthon/src/main/java/com/hackthon/service/QueueService.java
@@ -1,5 +1,8 @@
 package com.hackthon.service;
 
+import java.util.ArrayList;
+import java.util.Queue;
+
 public interface QueueService {
 
 	int addMessageToQueue(String queueName, String message);
@@ -9,4 +12,12 @@ public interface QueueService {
 	int addQueue(String queueName, int maxSize);
 	
 	int removeAll();
+	
+	ArrayList<String> listQueues();
+	
+	Queue<String> browseQueue(String queueName);
+	
+	int removeItemFromQueue(String queueName);
+	
+	
 }
