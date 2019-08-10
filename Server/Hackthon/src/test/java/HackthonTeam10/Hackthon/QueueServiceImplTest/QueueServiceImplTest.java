b3e3ebb159652eb7423855eb515b2f8caaf07421
Sweety Rayana <sekhar1329@gmail.com>
2019-08-10 15:49:40 +0530

    test case

--- Server/Hackthon/src/test/java/HackthonTeam10/Hackthon/QueueServiceImplTest/QueueServiceImplTest.java
+++ Server/Hackthon/src/test/java/HackthonTeam10/Hackthon/QueueServiceImplTest/QueueServiceImplTest.java
@@ -1,9 +1,10 @@
 package HackthonTeam10.Hackthon.QueueServiceImplTest;
 
+import org.junit.Test;
+
 import com.hackthon.serviceImpl.QueueServiceImpl;
 
 import junit.framework.TestCase;
-import org.junit.Test;
 
 public class QueueServiceImplTest extends TestCase {
 	
@@ -13,6 +14,11 @@ public class QueueServiceImplTest extends TestCase {
 	@Test
 	public void testAddQueue() {
 		assertEquals(0,queueService.addQueue("test", 10));
+	}
+	
+	@Test
+	public void testAddElementToQueue() {
+		assertEquals(0,queueService.addMessageToQueue("one", "Hello"));
 	}
 	
 }
